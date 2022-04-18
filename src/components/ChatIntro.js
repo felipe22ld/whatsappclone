b8ee8fdfe0f7ac6a08ee07959bb414e92e8b7790
Felipe Lima <felipedias@hotmail.com.br>
2022-04-17 21:27:45 -0300

    first commit

---
+++ src/components/ChatIntro.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './ChatIntro.css'
+
+export default () => {
+    return(
+        <div>
+            <div className="chatIntro">
+                <img src="https://imgmedia.libero.pe/670x396/libero/original/2022/03/09/6228a7a8c97b7f495a664aa9.webp" alt="whatsapp"/>
+                <h1>Matenha seu celular conectado</h1>
+                <h2>O Whatsapp conecta ao seu telefone para sinronizar suas mensagens. Para reduzir o uso de dados, conecte seu telefone a uma rede wi-fi. </h2>
+            </div>
+        </div>
+    )
+}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCK-KzgzfqNCW_PEsHHN8K4DAOp48T_Jho",
+    authDomain: "whatsappclone-986d6.firebaseapp.com",
+    projectId: "whatsappclone-986d6",
+    storageBucket: "whatsappclone-986d6.appspot.com",
+    messagingSenderId: "80120797679",
+    appId: "1:80120797679:web:77ff7e346d356b5321be1a"
+  };
